10c7be23942390354a68be3c91731bd747d4db39
NandkishorRL <nandkishormetange481@gmail.com>
2025-07-17 09:35:38 +0530

    Advice App code is added

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = { advice: "" };
+
+  componentDidMount() {
+    this.fetchAdvice();
+  }
+
+  fetchAdvice = () => {
+    axios
+      .get("https://api.adviceslip.com/advice")
+      .then((response) => {
+        const { advice } = response.data.slip;
+
+        this.setState({ advice });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  render() {
+    const { advice } = this.state;
+
+    return (
+      <div className="app">
+        <div className="card">
+          <h1 className="heading">{advice}</h1>
+
+          <button className="button" onClick={this.fetchAdvice}>
+            <span> Give me a Advice </span>
+          </button>
+
+          <button
+            className="button btn-secondary"
+            onClick={() => {
+              navigator.clipboard.writeText(advice);
+              alert("Copied to Clipboard!");
+            }}
+          >
+            <span>Copy to Clipboard</span>
+          </button>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
